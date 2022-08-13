12857bc0d1914db21965c20cd827caa429fb450c
elizbar <elizbar>
2022-08-13 15:05:19 +0400

    Initial Commit

---
+++ Task_22_elizbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task_22_elizbar
+//
+//  Created by alta on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task_22_elizbar/Task_22_elizbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_22_elizbarApp.swift
+//  Task_22_elizbar
+//
+//  Created by alta on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_22_elizbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
